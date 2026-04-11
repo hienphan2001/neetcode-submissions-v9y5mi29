a4b3b883944e3af7861d0eea2fc10546dad40e3d
Phan Thien Vinh Hien <t_phun@ibsu.edu.ge>
2026-04-11 12:47:41 +0400

    Bulk sync: 9 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character, Integer> dic_1 = new HashMap<>();
+        HashMap<Character, Integer> dic_2 = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (!dic_1.containsKey(s.charAt(i))) {
+                dic_1.put(s.charAt(i), 1);
+            } else 
+                dic_1.put(s.charAt(i), dic_1.get(s.charAt(i)) + 1);
+        }
+
+        for (int j = 0; j < t.length(); j++) {
+            if (!dic_2.containsKey(t.charAt(j))) {
+                dic_2.put(t.charAt(j), 1);
+            } else 
+                dic_2.put(t.charAt(j), dic_2.get(t.charAt(j)) + 1);
+        }
+
+        if (dic_1.equals(dic_2)) {
+            return true;
+        } else return false;
+    }
+}
