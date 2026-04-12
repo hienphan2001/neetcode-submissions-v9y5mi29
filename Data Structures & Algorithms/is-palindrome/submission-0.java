a6abf42227f131d37ecf09001b946ab5bedcd08d
Phan Thien Vinh Hien <t_phun@ibsu.edu.ge>
2026-04-12 22:00:10 +0400

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int l = 0, r=s.length() - 1;
+
+        while (l < r) {
+            while (l < r && !isAlphanumerical(s.charAt(l))) {
+                l++;
+            }
+            while (r>l && !isAlphanumerical(s.charAt(r))) {
+                r--;
+            }
+            if (Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r)))
+                return false;
+
+            l++; 
+            r--;
+        }
+
+        return true;
+    }
+
+    private boolean isAlphanumerical(char c) {
+        return (c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z' || c >= '0' && c <= '9');
+    }
+}
