c11e3924d3552083410ab197fcc7df1eaa614ca3
Phan Thien Vinh Hien <t_phun@ibsu.edu.ge>
2026-04-16 11:45:56 +0400

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] charsOrigin = s.toCharArray();
+        char[] charsCheck = t.toCharArray();
+        Arrays.sort(charsOrigin);
+        Arrays.sort(charsCheck);
+
+        String origin = String.valueOf(charsOrigin);
+        String check = String.valueOf(charsCheck);
+
+        return origin.equals(check);
+    }
+}
